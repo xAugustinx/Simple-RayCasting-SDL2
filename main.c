fd501e8f48ea891842d4d4744fb1747e04dd9061
𝙰𝚞𝚐𝚞𝚜𝚝𝚒𝚗 <xaugustinx12@gmail.com>
2025-12-26 20:04:17 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -20,10 +20,28 @@ int inputNaLiczby[3][4] =
     {0,        0,  -1, 1},
 };
 
+void nowyKlocek() {
+    int nowyX;
+    int nowyY;
+    for (int y = 0; y < 20; y++) {
+        for (int x = 0; x < 20; x++) {printf("%d ",tablica[y][x]);}
+        printf("\n");}
+
+    printf("\n%s\n","Wartość x: ");
+    scanf("%d",&nowyX);
+    system("clear");
+    printf("\n%s\n","Wartość y: ");
+    scanf("%d",&nowyY);
+
+    tablica[nowyY][nowyX] = 1;
+
+}
 
 int main() {
     for (int y = 0; y < 20; y++) { for (int x = 0; x < 20; x++) { tablica[y][x] = 0; }}
     tablica[5][10] = 1;
+
+    tablica[15][10] = 1;
 
     SDL_Window* meowOkno = SDL_CreateWindow ("FajneOkno",800,800,800,800,SDL_WINDOW_SHOWN);
     SDL_Renderer* meowRender = SDL_CreateRenderer (meowOkno, -1, SDL_RENDERER_ACCELERATED);
@@ -41,16 +59,15 @@ int main() {
             float stepX = cos(gracz[2] + (i * 0.1));
             float stepY = sin(gracz[2] + (i * 0.1));
 
-
             int timer = 0;
             while (1) {
 
-                if (pozycjeStartowe[0] >= 200 || pozycjeStartowe[1] >= 200 || pozycjeStartowe[0] <= 0 || pozycjeStartowe[1] <= 0 || colorDoTablicy >= 255 + 1   ) { colorDoTablicy = 0;  break;}
-                else if (tablica[ (int)pozycjeStartowe[0]/10 ][ (int)pozycjeStartowe[1]/10] != 0  ) { break;}
+                if (pozycjeStartowe[0] >= 200 || pozycjeStartowe[1] >= 200 || pozycjeStartowe[0] <= 0 || pozycjeStartowe[1] <= 0 || colorDoTablicy >= 255 + 2   ) { colorDoTablicy = 0;  break;}
+                else if (tablica[ (int)round(pozycjeStartowe[0]/10) ][  (int)round(pozycjeStartowe[1]/10) ] != 0  ) { break;}
 
                 if (timer) {pozycjeStartowe[0] += stepY;}
                 else { pozycjeStartowe[1] += stepX ;}
-                colorDoTablicy+= -1;
+                colorDoTablicy+= -2;
                 timer++;
                 if (timer > 1) { timer = 0;}
             }
@@ -70,20 +87,22 @@ int main() {
             if (meowEvent.type == SDL_QUIT ) { turnOn = 0; }
             else if (meowEvent.type == SDL_KEYDOWN)
             {
-                if (meowEvent.key.keysym.sym == SDLK_c ) {turnOn = 0;}
+                if (meowEvent.key.keysym.sym == SDLK_ESCAPE ) {turnOn = 0;}
                 else if (meowEvent.key.keysym.sym == SDLK_LEFT) {gracz[2] -= 0.30; if (gracz[2] < 0) { gracz[2] = 6.283;   };}
                 else if (meowEvent.key.keysym.sym == SDLK_RIGHT) {gracz[2] += 0.30;   if (gracz[2] > 6.283) { gracz[2] = 0;   };}
+                else if (meowEvent.key.keysym.sym == SDLK_e) { nowyKlocek();   }
                 for (int x = 0; x < 4; x++) {
                     if (meowEvent.key.keysym.sym == inputNaLiczby[0][x]) {
-                        gracz[0] += inputNaLiczby[1][x];
-                        gracz[1] += inputNaLiczby[2][x];
+                        int mango67One = (int)((gracz[0] + (float)inputNaLiczby[1][x]/2)/10);
+                        int mango67Two = (int)((gracz[1] + (float)inputNaLiczby[2][x]/2)/10);
 
-                        printf("%f",gracz[0]);
-                        printf("%c", ' ');
-                        printf(" %f",gracz[1]);
-                        printf("\n");
+                        if (!tablica[mango67One][mango67Two] ) {
 
-                        break;
+                            gracz[0] += (float)inputNaLiczby[1][x]/2;
+                            gracz[1] += (float)inputNaLiczby[2][x]/2;
+                            printf("%f%c%f%c%f\n",gracz[0],' ',gracz[1],' ',gracz[2]);
+                            break;
+                        }
                     }
                 }
             }
